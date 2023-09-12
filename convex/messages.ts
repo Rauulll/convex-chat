2c090f7485473f3af83cfa4bfbf1063330ef2a2b
Rauulll <nyaokeraul003@gmail.com>
2023-09-12 12:55:21 +0300

    Creating the likes button

--- convex/messages.ts
+++ convex/messages.ts
@@ -6,12 +6,22 @@ export const list = query({
   handler: async (ctx) => {
     // Grab the most recent messages.
     const messages = await ctx.db.query("messages").order("desc").take(100);
-    // Reverse the list so that it's in a chronological order.
-    return messages.reverse().map((message) => ({
-      ...message,
-      // Format smileys
-      body: message.body.replaceAll(":)", "😊"),
-    }));
+    const messagesWithLikes = await Promise.all(
+      messages.map(async (message) => {
+        // Find the likes for each message
+        const likes = await ctx.db
+          .query("likes")
+          .withIndex("byMessageId", (q) => q.eq("messageId", message._id))
+          .collect();
+        // Join the count of likes with the message data
+        return {
+          ...message,
+          likes: likes.length,
+        };
+      })
+    );
+    // Reverse the list so that it's in chronological order.
+    return messagesWithLikes.reverse();
   },
 });
 
@@ -20,5 +30,12 @@ export const send = mutation({
   handler: async (ctx, { body, author }) => {
     // Send a new message.
     await ctx.db.insert("messages", { body, author });
+  },
+});
+
+export const like = mutation({
+  args: {liker: v.string(), messageId: v.id("messages")},
+  handler: async (ctx, {liker, messageId}) => {
+    await ctx.db.insert("likes", {liker, messageId});
   },
 });
